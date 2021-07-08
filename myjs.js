25ee9db8ed333ebb969582f14ceaf40497f90baf
Chaitra Kulkarni <37969671+Chaitra-Kulkarni@users.noreply.github.com>
2021-07-08 10:58:59 +0530

    initial commit

---
+++ myjs.js
@@ -0,0 +1,65 @@
+// Arrow Functions
+
+/* const greet = () => `hello, world`;
+const result = greet();
+console.log(result); */
+
+
+//Callbacks & foreach
+// let people = ['chaitra', 'kulkarni', 'patil', 'kishor'];
+
+//Declare callback function and use it later
+// const logPerson = (person, index) => {
+//     console.log(index, person);
+// };
+// people.forEach(logPerson);
+
+//Use call back function on the go
+// people.forEach((person, index) => {
+//     console.log(index, person);
+// });
+
+// people.forEach(person => {
+//     console.log(person);
+// });
+
+// const ul = document.querySelector('.people');
+// const people = ['mario', 'chun-li', 'chaitra', 'kulkarni', 'patil', 'kishor'];
+//people.push('nanesy', 'param', 'kajal');
+//people.pop();
+//people.unshift('satya', 'shankar');
+// let html = ``;
+// people.forEach(person => {
+//     //create html template
+//     html += `<h2>${person}</h2>`;
+// });
+// console.log(html);
+// ul.innerHTML = html;
+
+
+//Object Literals
+
+// let user = {
+//     name: 'Chaitra',
+//     age: 25,
+//     email: 'chaitrark22.com',
+//     location: 'Bangalore',
+//     blogs: ['Learn JS in 30 days', 'Learn the basics of React in 14 days', 'Skills to be a Web Developer in 2021'],
+//     logBlogs: function() {
+//         //console.log(this.blogs);
+//         console.log('These are the following blogs that are written by Chaitra: ');
+//         this.blogs.forEach((blog,index) => {
+//             console.log(index,blog);
+//         });
+//     }
+// }
+
+// user.logBlogs();
+
+// const paragraph = document.querySelector('p'); // This targets the first paragraph from HTML page.
+// const paragraphs = document.querySelectorAll(p); // This targets all the paragraphs from HTML page.
+// const heading = document.getElementById('hero'); // This targets the element of #hero ID.
+// const styling = document.getElementsByClassName('container'); // This targets all the elements of .container class.
+// const tagName = document.getElementsByTagName('h2'); // This targets all the elements of h2 tag..
+
+
